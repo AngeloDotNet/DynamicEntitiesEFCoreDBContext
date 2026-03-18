dea059d149ed872e70113c72edc3402e44feca23
Angelo Pirola <angelo9871@gmail.com>
2026-03-18 08:48:54 +0100

    Add Product entity implementing IEntity interface
    Introduced Product class in PluginProduct.Entities namespace.
    Implements IEntity from App.Abstractions.
    Includes Id, Name (non-null default), and Price properties.

---
+++ src/PluginProduct/Entities/Product.cs
@@ -0,0 +1,10 @@
+﻿using App.Abstractions;
+
+namespace PluginProduct.Entities;
+
+public class Product : IEntity
+{
+	public int Id { get; set; }
+	public string Name { get; set; } = default!;
+	public decimal Price { get; set; }
+}
