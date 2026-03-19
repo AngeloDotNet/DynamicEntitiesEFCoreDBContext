e969e7eb8f276af5dd211fa7375ea59814156c17
Angelo Pirola <angelo9871@gmail.com>
2026-03-19 08:54:29 +0100

    Switch to PluginLoader for plugin management
    Removed McMaster.NETCore.Plugins dependency and refactored Program.cs to use PluginLoader for loading plugin assemblies. Registered plugin assemblies directly in DI container. Updated console instructions to reflect new plugin loading process.

--- src/App.Host/Program.cs
+++ src/App.Host/Program.cs
@@ -1,31 +1,27 @@
 ﻿//Console.WriteLine("Hello, World!");
+using System.Reflection;
 using App.Host.Data;
 using App.Host.PluginManagement;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System.Reflection;
 
-var host = Host.CreateDefaultBuilder(args)
-	.ConfigureServices((context, services) =>
-	{
-		// CARTELLA PLUGIN: può essere sovrascritta con env var PLUGIN_FOLDER
-		var pluginFolder = Environment.GetEnvironmentVariable("PLUGIN_FOLDER") ?? Path.Combine(AppContext.BaseDirectory, "plugins");
+var host = Host
+    .CreateDefaultBuilder(args)
+    .ConfigureServices((context, services) =>
+    {
+        var pluginFolder = Environment.GetEnvironmentVariable("PLUGIN_FOLDER") ?? Path.Combine(AppContext.BaseDirectory, "plugins");
+        var pluginAssemblies = PluginLoader.LoadPlugins(pluginFolder).ToArray();
 
-		var pluginManager = new PluginManager();
-		pluginManager.LoadPluginsFromFolder(pluginFolder);
+        services.AddSingleton<IEnumerable<Assembly>>(pluginAssemblies);
 
-		services.AddSingleton(pluginManager);
-		services.AddSingleton<IEnumerable<Assembly>>(sp => sp.GetRequiredService<PluginManager>().Assemblies);
+        var conn = context.Configuration.GetConnectionString("DefaultConnection") ?? "Server=(localdb)\\mssqllocaldb;Database=MyApp;Trusted_Connection=True;";
 
-		var conn = context.Configuration.GetConnectionString("DefaultConnection") ?? "Server=(localdb)\\mssqllocaldb;Database=MyApp;Trusted_Connection=True;";
+        services.AddDbContext<MyDbContext>(options =>
+        {
+            options.UseSqlServer(conn);
+        });
+    }).Build();
 
-		services.AddDbContext<MyDbContext>(options =>
-		{
-			options.UseSqlServer(conn);
-		});
-	})
-	.Build();
-
-Console.WriteLine("Host built. Run your application or use dotnet ef with --startup-project MyApp.Host");
+Console.WriteLine("Host built. Copia i plugin nella cartella plugins e usa dotnet ef con --startup-project se necessario.");
